d48e52cdc4304cedb8e235a387b5eecd88e27440
jakobakoba <jakobakoba@gmail.com>
2023-08-23 17:55:44 +0600

    animation

--- app/src/main/java/com/bor96dev/sunset/MainActivity.kt
+++ app/src/main/java/com/bor96dev/sunset/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.bor96dev.sunset
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bor96dev.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scene.setOnClickListener {
+            startAnimation()
+        }
     }
+
+    private fun startAnimation() {
+        val sunYStart = binding.sun.top.toFloat()
+        val sunYEnd = binding.sky.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+
+        heightAnimator.start()
+    }
+
+
 }
